8aab39939f981b1f0a4cb7095a8bdd13b3eb8657
Yashkalra12 <yash1578.be21@chitkara.edu.in>
2025-04-13 23:58:15 +0530

    Project setup

---
+++ Cineflix-app/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-centre items-center">
+      <Text>Welcome</Text>
+    </View>
+  );
+}
