838bc09b92aa745d437e47179f7b8fa31f45616b
Yashkalra12 <yash1578.be21@chitkara.edu.in>
2025-04-20 19:00:21 +0530

    fix:nativewind configuration

--- Cineflix-app/app/index.tsx
+++ Cineflix-app/app/index.tsx
@@ -2,9 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-centre items-center">
-      <Text className="text-5xl text-accent font-bolt">Welcome</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-red-300 font-bold">Yash Kalra</Text>
     </View>
   );
 }
-yash
--- sensai/components/ClerkProviderWrapper.js
+++
@@ -1,7 +0,0 @@
-"use client";  // ✅ Required for Clerk
-
-import { ClerkProvider } from "@clerk/clerk-react";
-
-export default function ClerkProviderWrapper({ children }) {
-  return <ClerkProvider>{children}</ClerkProvider>;
-}
